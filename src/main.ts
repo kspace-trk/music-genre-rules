cab9ba2863996a5330cc6446b174888ad04e7213
kspace-trk <keigonakao.0825@gmail.com>
2023-12-02 21:06:46 +0900

    add: diff notes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import one_measure_repeating_notes from './json/one_measure_repeating_notes.json';
 import two_measure_repeating_notes from './json/two_measure_repeating_notes.json';
 import four_measure_repeating_notes from './json/four_measure_repeating_notes.json';
+import one_measure_repeating_notes_diff from './json/mutationData/one_measure_repeating_notes_diff.json';
+import two_measure_repeating_notes_diff from './json/mutationData/two_measure_repeating_notes_diff.json';
 
 export default {
   one_measure_repeating_notes,
   two_measure_repeating_notes,
   four_measure_repeating_notes,
+  one_measure_repeating_notes_diff,
+  two_measure_repeating_notes_diff
 }
